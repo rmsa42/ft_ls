a06809778dae38935e9aa61677f70d772e3cd595
rmsa42 <rui.soares.almeida@outlook.pt>
2025-10-19 01:36:31 +0100

    Closing dirp. Adding DEBUG logs

--- main.c
+++ main.c
@@ -1,12 +1,20 @@
 #include "libft.h"
 #include <dirent.h>
 
+#define DEBUG 1
+
 void print_dir(DIR *dirp) {
 	struct dirent *dir = NULL;
 
+	#if DEBUG == 1
+	ft_printf("Listing Dir:\n");
+	#endif
+	dir = readdir(dirp);
+	ft_printf("%s", dir->d_name);
 	while ((dir = readdir(dirp)) != NULL) {
-		ft_printf("%s\n", dir->d_name);
+		ft_printf(" %s", dir->d_name);
 	}
+	ft_printf("\n");
 }
 
 int main(int argc, char **argv) {
@@ -20,7 +28,15 @@ int main(int argc, char **argv) {
 	}
 
 	dirp = opendir(dir_path);
+	#if DEBUG == 1
+	ft_printf("Opened Dir\n");
+	#endif
 
 	print_dir(dirp);
+
+	#if DEBUG == 1
+	ft_printf("Closing Dir\n");
+	#endif
+	closedir(dirp);
 	return (0);
 }
